252a475a6e9b8eee738a8514c9be72fcd4a60358
OierGman <75170798+OierGman@users.noreply.github.com>
2022-01-10 14:12:13 +0000

    Working data storing and CSV exporter

--- Housing Prediction Value Tool/Form1.Designer.cs
+++ Housing Prediction Value Tool/Form1.Designer.cs
@@ -31,33 +31,116 @@ namespace Housing_Prediction_Value_Tool
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textboxRooms = new System.Windows.Forms.TextBox();
+            this.textboxValue = new System.Windows.Forms.TextBox();
+            this.textboxType = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.labelNumData = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(149, 22);
+            this.dataGridView1.Location = new System.Drawing.Point(423, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 25;
-            this.dataGridView1.Size = new System.Drawing.Size(489, 319);
+            this.dataGridView1.Size = new System.Drawing.Size(365, 319);
             this.dataGridView1.TabIndex = 0;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(563, 347);
+            this.button1.Location = new System.Drawing.Point(423, 337);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(365, 27);
             this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
+            this.button1.Text = "EXPORT TO CSV";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Number of Rooms";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 204);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(409, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Property Type (h-house, th-terraced house, f-flat, b-bungalow, u-undefined)";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Value of Property";
+            // 
+            // textboxRooms
+            // 
+            this.textboxRooms.Location = new System.Drawing.Point(12, 96);
+            this.textboxRooms.Name = "textboxRooms";
+            this.textboxRooms.Size = new System.Drawing.Size(100, 23);
+            this.textboxRooms.TabIndex = 5;
+            // 
+            // textboxValue
+            // 
+            this.textboxValue.Location = new System.Drawing.Point(12, 160);
+            this.textboxValue.Name = "textboxValue";
+            this.textboxValue.Size = new System.Drawing.Size(100, 23);
+            this.textboxValue.TabIndex = 6;
+            // 
+            // textboxType
+            // 
+            this.textboxType.Location = new System.Drawing.Point(12, 222);
+            this.textboxType.Name = "textboxType";
+            this.textboxType.Size = new System.Drawing.Size(100, 23);
+            this.textboxType.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 337);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(405, 27);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "SUBMIT TO DATATABLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // labelNumData
+            // 
+            this.labelNumData.AutoSize = true;
+            this.labelNumData.Location = new System.Drawing.Point(12, 12);
+            this.labelNumData.Name = "labelNumData";
+            this.labelNumData.Size = new System.Drawing.Size(115, 15);
+            this.labelNumData.TabIndex = 9;
+            this.labelNumData.Text = "Number of Entries: 0";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(799, 376);
+            this.Controls.Add(this.labelNumData);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textboxType);
+            this.Controls.Add(this.textboxValue);
+            this.Controls.Add(this.textboxRooms);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
@@ -65,6 +148,7 @@ namespace Housing_Prediction_Value_Tool
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -72,6 +156,14 @@ namespace Housing_Prediction_Value_Tool
 
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textboxRooms;
+        private System.Windows.Forms.TextBox textboxValue;
+        private System.Windows.Forms.TextBox textboxType;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label labelNumData;
     }
 }
 
--- Housing Prediction Value Tool/Form1.cs
+++ Housing Prediction Value Tool/Form1.cs
@@ -17,17 +17,20 @@ namespace Housing_Prediction_Value_Tool
             InitializeComponent();
         }
 
+        DataTable tableData = new DataTable();
+
         private void Form1_Load(object sender, EventArgs e)
         {
-            DataTable dt = OperationsUtlity.createDataTable();
-            dataGridView1.DataSource = dt;
+            dataGridView1.DataSource = tableData;
+            tableData.Columns.Add("ROOMS", typeof(int));
+            tableData.Columns.Add("VALUE", typeof(int));
+            tableData.Columns.Add("HOUSE TYPE", typeof(string));
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            DataTable dt = OperationsUtlity.createDataTable();
             string filename = OpenSavefileDialog();
-            dt.ToCSV(filename);
+            tableData.ToCSV(filename);
         }
 
         private string OpenSavefileDialog()
@@ -42,10 +45,23 @@ namespace Housing_Prediction_Value_Tool
             if (dialogResult == DialogResult.OK)
             {
                 Filename = saveFileDialog.FileName;
-
             }
-
             return Filename;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int room = int.Parse(textboxRooms.Text);
+            int value = int.Parse(textboxValue.Text);
+            string type = textboxType.Text.ToUpper();
+
+            tableData.Rows.Add(room, value, type);
+
+            textboxRooms.Text = String.Empty;
+            textboxValue.Text = String.Empty;
+            textboxType.Text = String.Empty;
+
+            labelNumData.Text = "Number of Entries: " + tableData.Rows.Count;
         }
     }
 }
