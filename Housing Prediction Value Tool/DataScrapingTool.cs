bbaa0bed97ee8d2e995d4a8e1b664e94dd717a90
OierGman <75170798+OierGman@users.noreply.github.com>
2022-01-17 13:36:31 +0000

    Working on a scraping tool that will extract required information from popular housing sites like rightmove and zoopla to make data collection quicker and efficient

---
+++ Housing Prediction Value Tool/DataScrapingTool.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace Housing_Prediction_Value_Tool
+{
+    class DataScrapingTool
+    {
+        static void Main(string[] args)
+        {
+            GetUtilityAsync();
+        }
+
+        private static void GetUtilityAsync()
+        {
+            var url = "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E61307&sortType=6&propertyTypes=&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=";
+
+            var httpClient = new HttpClient();
+            var html = httpClient.GetStringAsync(url);
+
+            // var htmlDoc = new HtmlDocument();
+        }
+    }
+}
