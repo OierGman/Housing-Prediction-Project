cb461044c68ebe6f69a96f487d09cea5bb080890
OierGman <75170798+OierGman@users.noreply.github.com>
2022-01-20 13:37:45 +0000

    adding web scraper to main housing prediction tool

--- Housing Prediction Value Tool/Program.cs
+++ Housing Prediction Value Tool/Program.cs
@@ -14,10 +14,42 @@ namespace Housing_Prediction_Value_Tool
         [STAThread]
         static void Main()
         {
+
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // getPropertyInfo();
+        }
+
+
+        static void getPropertyInfo(string propertyType, int propertyRooms)
+        {
+            var url = "https://www.rightmove.co.uk/property-for-sale/find.html?locationIdentifier=REGION%5E61307&maxBedrooms=" + propertyRooms + "&minBedrooms=" + propertyRooms + "&propertyTypes=" + propertyType + "&includeSSTC=false&mustHave=&dontShow=&furnishTypes=&keywords=";
+
+            HtmlAgilityPack.HtmlWeb web = new HtmlAgilityPack.HtmlWeb();
+
+            HtmlAgilityPack.HtmlDocument doc = web.Load(url);
+
+            var propertyHtml = doc.DocumentNode.Descendants("div")
+                .Where(node => node.GetAttributeValue("class", "")
+                .Equals("l-searchResults")).ToList();
+
+            var propertyListItems = propertyHtml[0].Descendants("div")
+                .Where(node => node.GetAttributeValue("id", "")
+                .Contains("property-")).ToList();
+
+            foreach (var propertyListItem in propertyListItems)
+            {
+                // id
+                Console.WriteLine(propertyListItem.GetAttributeValue("id", ""));
+
+                // property value
+                Console.WriteLine(propertyListItem.Descendants("div")
+                    .Where(node => node.GetAttributeValue("class", "")
+                    .Equals("propertyCard-priceValue")).FirstOrDefault().InnerText.Trim('\t', '\n', '£', 'r'));
+            }
         }
     }
 }
